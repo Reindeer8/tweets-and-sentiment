f0fae975960b2100b73b39111bf202bcf18cd532
Reindeer8 <44758891+Reindeer8@users.noreply.github.com>
2021-09-21 15:26:32 +0300

    More changes to sentiment analyzer

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,23 +1,23 @@
+# In large part this implementation of sentiment analysis was inspired by
+# this article: 
+# https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk
+
 import nltk
 import re 
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import twitter_samples, stopwords
-nltk.download('stopwords')
+# nltk.download('stopwords')
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk import classify, NaiveBayesClassifier
 from random import shuffle
 from nltk.corpus import wordnet
-nltk.download('wordnet')
- testing
-# In large part this implementation of sentiment analysis was inspired by
-# this article: 
-# https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk
+# nltk.download('wordnet')
 
 class SentimentAnalyzer:
 
     def __init__(self, text:str = None):
-        self.train_naive_Bayes_classificator()
+        self.classifier = self.train_naive_Bayes_classificator()
         self.text = text
         if text:
             self.tokenize(text)
@@ -25,8 +25,7 @@ class SentimentAnalyzer:
         else:
             self.text = None
     
-    def _clean_tokens(tweet_tokens:list, stop_words:tuple=()):
-        print(stop_words)
+    def clean_tokens_and_lemmetize(self, tweet_tokens:list, stop_words:tuple=()):
         cleaned_tokens = []
 
         pos_dict = {'V':'v', 'N':'n'}
@@ -52,19 +51,25 @@ class SentimentAnalyzer:
 
         positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
         negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
-        print(positive_tweet_tokens[:10])
+
         positive_cleaned_tokens_list = []
         negative_cleaned_tokens_list = []
 
+
         for tokens in positive_tweet_tokens:
-            positive_cleaned_tokens_list.append(self.remove_noise(tokens, stop_words))
+            positive_cleaned_tokens_list.append(self.clean_tokens_and_lemmetize(tokens, stop_words))
 
         for tokens in negative_tweet_tokens:
-            negative_cleaned_tokens_list.append(self.remove_noise(tokens, stop_words))
+            negative_cleaned_tokens_list.append(self.clean_tokens_and_lemmetize(tokens, stop_words))
 
-        positive_tokens_for_model = self.get_tweets_for_model(positive_cleaned_tokens_list)
-        negative_tokens_for_model = self.get_tweets_for_model(negative_cleaned_tokens_list)
+        print('\nThis is the cleaned tokens list\n')
+        print(positive_cleaned_tokens_list[:10])
 
+        # puts all tokens in one pile, in dict format, where key is token followed by True
+        positive_tokens_for_model = self.format_tweets_for_model(positive_cleaned_tokens_list)
+        negative_tokens_for_model = self.format_tweets_for_model(negative_cleaned_tokens_list)
+
+        print(list(positive_tokens_for_model)[:10])
         positive_dataset = [(tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model]
 
         negative_dataset = [(tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model]
@@ -72,60 +77,61 @@ class SentimentAnalyzer:
         dataset = positive_dataset + negative_dataset
 
         shuffle(dataset)
+        print()
+        print(type(dataset))
+        print(len(dataset))
 
-        self.classifier = NaiveBayesClassifier.train(dataset).
+        self.train_data = dataset[:4000]
+        self.test_data = dataset[4000:]
+
+        return NaiveBayesClassifier.train(self.train_data)
 
 
-    def _get_tweets_for_model(cleaned_tokens_list):
+    def format_tweets_for_model(self, cleaned_tokens_list):
+        # reformats cleaned tokens in a format appropriate for the model
         for tweet_tokens in cleaned_tokens_list:
             yield dict([token, True] for token in tweet_tokens)
+            
         
-    def do_sentiment():    
+    def determine_sentiment(self, x:str = ''):
 
-        print("Accuracy is:", classify.accuracy(classifier, dataset))
 
         custom_tweets = ["I do not think Apple is a good company. Even more I belevie next quarter won't be their best",
         "Trust I seek and I am finding you",
         "This time it looks like lovely bag of shit",
         "The new gadget from Xtime is the shit"]
 
-        custom_tokens = [remove_noise(word_tokenize(custom_tweet)) for custom_tweet in custom_tweets]
+        custom_tokens = [self.clean_tokens_and_lemmetize(word_tokenize(custom_tweet)) for custom_tweet in custom_tweets]
 
-        sid = SentimentIntensityAnalyzer()
         for tweet_num, custom_tweet in enumerate(custom_tweets):
+            custom_tokens = tweet_analyzer.clean_tokens_and_lemmetize(word_tokenize(custom_tweet)) 
             print(custom_tweet) 
             print()
             print('With NaiveBayesClassifier...')
-            print(classifier.classify(dict([token, True] for token in custom_tokens[tweet_num])), sep = '\n')
+            print(self.classifier.classify(dict([token, True] for token in custom_tokens[tweet_num])), sep = '\n')
             print()
-            print('With Vader...')
-            print(sid.polarity_scores(custom_tweet))
 
+    def accuracy_info(self):
+        return classify.accuracy(self.classifier, self.test_data)
 
 if __name__ == "__main__":
-    stopwords.ensure_loaded()
-    
-    # print(dir(wordnet))
-    # print(wordnet.ADJ)
-    a = SentimentAnalyzer()
-    # do_sentiment()
 
-    custom_tweets = ["I do not think Apple is a good company. Even more I belevie next quarter won't be their best",
+    stopwords.ensure_loaded()
+    print(type(stopwords)) 
+    tweet_analyzer = SentimentAnalyzer()
+    
+    
+    custom_tweets = [
+        "I do not think Apple is a good company. Even more I belevie next quarter won't be their best",
         "Trust I seek and I am finding you",
         "This time it looks like lovely bag of shit",
         "The new gadget from Xtime is the shit",
-        "I do not like company Apple, but I do love Iphone"]
+        "I do not like company Apple, but I do love Iphone"
+        ]
 
-    # custom_tokens = [remove_noise(word_tokenize(custom_tweet)) for custom_tweet in custom_tweets]
+    print('Accuracy is:', tweet_analyzer.accuracy_info())
 
-    sid = SentimentIntensityAnalyzer()
-    for tweet_num, custom_tweet in enumerate(custom_tweets):
-            print()
-            print()
-            print(custom_tweet) 
-            # print()
-            # print('With NaiveBayesClassifier...')
-            # print(classifier.classify(dict([token, True] for token in custom_tokens[tweet_num])), sep = '\n')
-            print()
-            print('With Vader...')
-            print(sid.polarity_scores(custom_tweet))    
+    # sid = SentimentIntensityAnalyzer()
+    for custom_tweet in custom_tweets:
+        print(custom_tweet)
+        print(tweet_analyzer.determine_sentiment(custom_tweet))
