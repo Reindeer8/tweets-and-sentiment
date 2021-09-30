461dc1b08e4b49511c96edd193ef60052ea68507
ME <edmunds.miluns@gmail.com>
2021-09-30 04:02:33 +0300

    night

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,9 +1,11 @@
+from sentiment_analyzer import SentimentAnalyzer
 import tweepy
 from twitter_creditentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 from argument_parser import ArgumentParser
+from time import time
 
 
-API_TYPE = "basic"
+API_TYPE = "search"
 # other option would be "advanced" for full archive search
 # in the latter case parameter names should be adjusted accordingly.
 # Another way could be continuous or cursor
@@ -11,41 +13,86 @@ API_TYPE = "basic"
 
 class TweetSentimentBot:
     
-    def create_nested_class(self, the_class):
-        return TweetSentimentBot.the_class(self)
+    def __init__(self):
+        self.tweets_with_sentiment:list = []
+        self.ratio:dict = {
+            "Positive": 0,
+            "Negative": 0,
+            "Total"   : 0 
+        }
+        self.most_popular_tweet: dict = {}
+        self.get_search_parameters()
+        if not self.search_parameters:
+            raise Exception('No parameters provided')
+        self.analyzer = SentimentAnalyzer()
+        if API_TYPE == "basic":
+            tweets = self.basic_tweet_search_with_cursor()
+        else:
+            self.create_nested_stream(self)
+            self.auth = None
+            self.connect_to_api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)            
+            self.get_live_tweets_with_stream()
+            
+        for tweet in tweets:
+            print('\n\n')
+            print('Author: ' + tweet.author.name)
+            print('The tweet is truncated: ', tweet.truncated)
+            print('The text of tweet:\n' + tweet.full_text)
+        print()
+
+    def create_nested_stream(self, the_outer_class):
+        return self.StreamSentiment(the_outer_class)
 
     class StreamSentiment(tweepy.StreamListener):
         """The class handles communication with twitter using tweepy, 
         analyzes the sentiment and outputs the result.    
         """
-        def __init__(self):
-            super().__init__(self, the_bot)
+        def __init__(self, the_bot):
+            super().__init__(self)
             self.the_bot = the_bot
+            self.time_of_last_print = None
+            self.interval_between_outputs = 10
 
         def on_status(self, status):
-            print(status.full_text)
+            text = status.full_text
+            sentiment = self.the_bot.analyzer.determinet_sentiment(text)
+            self.the_bot.tweets_with_sentiment.append([text, sentiment])
+            self.the_bot.ratio[sentiment] += 1
+            self.the_bot.ratio["Total"] += 1
+            if not self.the_bot.most_popular_tweet or \
+                self.the_bot.most_popular_tweet.get("Retweets", 0):
+                self.the_bot.mostpopular_tweet = {
+                    "Text": text,
+                    "Sentiment": sentiment,
+                    "Retweets": status.full_text
+                }
+            if not self.time_of_last_print:
+                self.time_of_last_print = time.time()
+            current_time = time.time()
+            if self.inteval_between_outputs >= self.time_of_last_print - current_time:
+                return
+            mpt = self.the_bot.most_popular_tweet
+            ratio = self.the_bot.ratio
+            print()
+            print()
+            print()
+            print(mpt["Text"])
+            print()
+            print("Retweeets:", mpt["Retweets"])
+            print("Sentiment:", mpt["Sentiment"])
+            print()
+            print()
+            num_of_pluses = round(20 * ratio["Positive"] / ratio["Total"])
+            num_of_minuses = 20 - num_of_pluses
+            print(f"P {ratio['Positive']}" + "+"*num_of_pluses + " " + "-" * num_of_minuses + f"{ratio['Negative']} N")
+            print()
             # yield status.text
 
         def on_error(self, status_code):
             if status_code == 420:
                 #returning False in on_error disconnects the stream
                 return False
-
-    class CursorSentiment():
-        def __init__(api, search_parameters = None):
-            if not api:
-                raise Exception('No api to talk to')
-            tweet_list = []
-            for tweet in tweepy.Cursor(api.search, q = search_parameters['q'], tweet_mode = 'extended').items(10):
-                tweet_list.append(tweet)        
             
-    def __init__(self):
-
-        self.search_parameters = {}
-        self.auth = None
-        self.connect_to_api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)   
-
-
     def get_search_parameters(self):
         """Parses arguments using the argument parser class"""
         parser = ArgumentParser()
@@ -60,7 +107,6 @@ class TweetSentimentBot:
             auth, 
             wait_on_rate_limit=True,
             wait_on_rate_limit_notify=True)
-
     
     def basic_tweet_search_with_cursor(self):
 
@@ -77,14 +123,14 @@ class TweetSentimentBot:
         twitter_listener = TweetSentimentBot.StreamSentiment(self.api)
         myStream = tweepy.Stream(auth = self.api.auth, listener=twitter_listener, tweet_mode = 'extended')
         print('Starting the stream')
-        myStream.filter(track=["one"], languages = ["en"])
+        myStream.filter(track=["ufo"], languages = ["en"])
         
 
 if __name__ == '__main__':
 
-    from pprint import pprint
-
+    
     the_bot = TweetSentimentBot()
+    """
     the_bot.get_search_parameters()
 
     if API_TYPE == "basic":
@@ -100,6 +146,7 @@ if __name__ == '__main__':
         print('The text of tweet:\n' + tweet.full_text)
     print()
     #pprint(tweets[0]._json)
+    """
 
     '''
     auth = tweepy.OAuthHandler( CONSUMER_KEY, CONSUMER_SECRET)
